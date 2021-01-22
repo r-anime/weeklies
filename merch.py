280451e37de53049c96f6c7bfb8a644389c360a4
Durinthal <durinthal@gmail.com>
2021-01-21 21:24:49 -0500

    Add flairs to all threads, using configurable flair id

--- merch.py
+++ merch.py
@@ -7,6 +7,7 @@ c.read('config.ini')
 
 reddit = praw.Reddit(**c['Auth'])
 subreddit = reddit.subreddit(c['Options']['subreddit'])
+flair_id = c['Options']['flair_weekly_id']
 
 title = datetime.date.today().strftime('Merch Mondays Megathread - Week of %B %d, %Y')
 content = """Weekly thread devoted to any and all new merch you may have picked up. If you've got physical goods to show off, it belongs here!
@@ -15,7 +16,7 @@ Have a collection you've amassed that you'd like to show off instead? Don't be s
 
 You can also use this thread for questions or advice about buying or collecting anime merch (but remember that selling things is not allowed on /r/anime)."""
 
-post = subreddit.submit(title, selftext=content)
+post = subreddit.submit(title, selftext=content, flair_id=flair_id)
 post.disable_inbox_replies()
 post.mod.suggested_sort(sort='new')
 post.mod.distinguish()
